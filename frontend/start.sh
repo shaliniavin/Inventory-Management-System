e607a03f06a3f29b237827b66964c292862b91b8
shaliniavin <153045829+shaliniavin@users.noreply.github.com>
2025-12-17 13:36:49 +0530

    Create start.sh

---
+++ frontend/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Go to frontend folder
+cd frontend
+
+# Install dependencies
+npm install
+
+# Build the app
+npm run build
+
+# Start the app on the port Railway/Railpack provides
+npm start
